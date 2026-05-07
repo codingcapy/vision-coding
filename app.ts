40771ddb47c6b92e1c1a3e025b03457286f3e90c
PaulArkhet <spkim0921@gmail.com>
2026-05-07 16:13:40 -0700

    feat: build bun hono server and vite react client

---
+++ app.ts
@@ -0,0 +1,46 @@
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { compress } from "hono/compress";
+import { logger } from "hono/logger";
+import { secureHeaders } from "hono/secure-headers";
+import { serveStatic } from "hono/bun";
+import { serve } from "@hono/node-server";
+
+const app = new Hono();
+
+app.use("*", logger());
+app.use("*", compress());
+app.use("*", secureHeaders());
+app.use(
+  "*",
+  cors({
+    origin: ["http://localhost:5173", "https://visioncoding.up.railway.app"],
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+
+const PORT = parseInt(process.env.PORT!) || 3333;
+
+const apiRoutes = app.basePath("/api/v0");
+
+export type ApiRoutes = typeof apiRoutes;
+export default app;
+
+app.use("/*", serveStatic({ root: "./frontend/dist" }));
+app.get("/*", async (c) => {
+  try {
+    const indexHtml = await Bun.file("./frontend/dist/index.html").text();
+    return c.html(indexHtml);
+  } catch (error) {
+    console.error("Error reading index.html:", error);
+    return c.text("Internal Server Error", 500);
+  }
+});
+
+const server = serve({
+  port: PORT,
+  fetch: app.fetch,
+});
+console.log("Server running on port", PORT);
